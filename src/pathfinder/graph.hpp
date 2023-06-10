7cd51deee5197ef68c026b0f842122853ef8c339
Max Smagin <lazlogogalac777@gmail.com>
2023-06-10 16:10:59 +0300

    Moved non-templated graph definitions out of graph

--- src/pathfinder/graph.hpp
+++ src/pathfinder/graph.hpp
@@ -9,9 +9,7 @@
 #include <unordered_map>
 #include <unordered_set>
 
-template <class GraphGenerator>
-class Graph {
-public:
+namespace Graphs {
     using Vertex = int;
     using DistType = int;
 
@@ -26,8 +24,12 @@ public:
     static DistType constexpr kMinRandomWeight = 1;
     static DistType constexpr kMaxRandomWeight = 10;
     static DistType constexpr kDistError = -1;
+};
 
-    Graph(StdRepresentation const& graph)
+template <class GraphGenerator>
+class Graph {
+public:
+    Graph(Graphs::StdRepresentation const& graph)
         : _graph(graph)
         , _vertex_count(graph.size())
     {
@@ -41,44 +43,44 @@ public:
         return _vertex_count;
     }
 
-    VertexIterator begin()
+    Graphs::VertexIterator begin()
     {
         return _graph.begin();
     }
 
-    VertexIterator end()
+    Graphs::VertexIterator end()
     {
         return _graph.end();
     }
 
-    VertexConstIterator begin() const
+    Graphs::VertexConstIterator begin() const
     {
         return _graph.begin();
     }
 
-    VertexConstIterator end() const
+    Graphs::VertexConstIterator end() const
     {
         return _graph.end();
     }
 
-    VertexAdjacentIterator adjacent_begin(Vertex v)
+    Graphs::VertexAdjacentIterator adjacent_begin(Graphs::Vertex v)
     {
         auto it = _graph.find(v);
-        return it == _graph.end() ? VertexAdjacentIterator(nullptr) : it->second.begin();
+        return it == _graph.end() ? Graphs::VertexAdjacentIterator(nullptr) : it->second.begin();
     }
 
-    VertexAdjacentIterator adjacent_end(Vertex v)
+    Graphs::VertexAdjacentIterator adjacent_end(Graphs::Vertex v)
     {
         auto it = _graph.find(v);
-        return it == _graph.end() ? VertexAdjacentIterator(nullptr) : it->second.end();
+        return it == _graph.end() ? Graphs::VertexAdjacentIterator(nullptr) : it->second.end();
     }
 
-    VertexAdjacentConstIterator adjacent_begin(Vertex v) const
+    Graphs::VertexAdjacentConstIterator adjacent_begin(Graphs::Vertex v) const
     {
         return adjacent_begin(v);
     }
 
-    VertexAdjacentConstIterator adjacent_end(Vertex v) const
+    Graphs::VertexAdjacentConstIterator adjacent_end(Graphs::Vertex v) const
     {
         return adjacent_end(v);
     }
@@ -89,13 +91,13 @@ public:
     }
 
 private:
-    StdRepresentation _graph;
+    Graphs::StdRepresentation _graph;
     size_t _vertex_count {};
 };
 
 class FullGraph : Graph<FullGraph> {
 public:
-    static void fill(StdRepresentation& graph, size_t vertex_count)
+    static void fill(Graphs::StdRepresentation& graph, size_t vertex_count)
     {
         graph.reserve(vertex_count);
 
@@ -106,14 +108,14 @@ public:
                 }
 
                 graph[v][adjacent] = graph[adjacent][v] != 0 ? graph[adjacent][v]
-                                                             : util::getRandomNumber(kMinRandomWeight, kMaxRandomWeight);
+                                                             : util::getRandomNumber(Graphs::kMinRandomWeight, Graphs::kMaxRandomWeight);
             }
         }
     }
 
     static Graph<FullGraph> generate(size_t vertex_count)
     {
-        StdRepresentation graph;
+        Graphs::StdRepresentation graph;
         fill(graph, vertex_count);
         return { graph };
     }
@@ -132,7 +134,7 @@ public:
 
         // Random Walk Approach.
         // Inspired by: https://stackoverflow.com/a/14618505
-        std::vector<Vertex> available;
+        std::vector<Graphs::Vertex> available;
         available.reserve(vertex_count);
         for (auto i = 0uz; i < vertex_count; ++i) {
             available.push_back(i);
@@ -144,7 +146,7 @@ public:
         available.erase(available.begin() + current_index);
         visited[current_vertex] = true;
 
-        StdRepresentation graph;
+        Graphs::StdRepresentation graph;
 
         while (!available.empty()) {
             auto const adjacent_index = util::getRandomNumber(0uz, available.size() - 1);
@@ -168,15 +170,15 @@ public:
         return { graph };
     }
 
-    static void addEdgeWithRandomWeight(StdRepresentation& graph, Vertex u, Vertex v)
+    static void addEdgeWithRandomWeight(Graphs::StdRepresentation& graph, Graphs::Vertex u, Graphs::Vertex v)
     {
-        auto const weight = util::getRandomNumber(kMinRandomWeight, kMaxRandomWeight);
+        auto const weight = util::getRandomNumber(Graphs::kMinRandomWeight, Graphs::kMaxRandomWeight);
         graph[u][v] = weight;
         graph[v][u] = weight;
     }
 
 private:
-    static void addRandomEdges(StdRepresentation& graph, size_t edge_count)
+    static void addRandomEdges(Graphs::StdRepresentation& graph, size_t edge_count)
     {
         for (auto i = 0uz; i < edge_count; ++i) {
             for (;;) {
@@ -201,19 +203,19 @@ public:
     static Graph<TreeGraph> generate(size_t vertex_count)
     {
         // Using Prüfer sequence to generate trees.
-        std::vector<Vertex> S = generatePrefill(vertex_count);
+        std::vector<Graphs::Vertex> S = generatePrefill(vertex_count);
 
         auto const n = S.size();
-        std::unordered_set<Vertex> L;
+        std::unordered_set<Graphs::Vertex> L;
         L.reserve(n + 2);
         for (auto i = 1uz; i < n + 2 + 1; ++i) {
             L.insert(i);
         }
 
-        StdRepresentation graph;
+        Graphs::StdRepresentation graph;
         for (auto i = 0uz; i < n; ++i) {
             auto const u = S.back();
-            std::vector<Vertex> diff;
+            std::vector<Graphs::Vertex> diff;
             std::ranges::set_difference(L, S, std::back_inserter(diff));
             auto const v = *std::min(diff.begin(), diff.end());
             S.pop_back();
@@ -227,10 +229,10 @@ public:
     }
 
 private:
-    static std::vector<Vertex> generatePrefill(size_t vertex_count)
+    static std::vector<Graphs::Vertex> generatePrefill(size_t vertex_count)
     {
         auto const to_pick_count = vertex_count - 2;
-        std::vector<Vertex> result(to_pick_count);
+        std::vector<Graphs::Vertex> result(to_pick_count);
 
         for (auto i = 0uz; i < to_pick_count; ++i) {
             result[i] = util::getRandomNumber(1uz, vertex_count);
